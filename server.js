ca73a44dd69bdd9c6ceae5ea416575fa88035465
Dustin <dustinsinkey1@gmail.com>
2016-02-05 16:57:54 -0800

    Updated server.js to use streamHandler and socket.io

--- server.js
+++ server.js
@@ -2,12 +2,23 @@ var express = require('express');
 var http = require('http');
 var mongoose = require('mongoose');
 var twitter = require('ntwitter');
+var config = require('./config');
+var streamHandler = require('./utils/streamHandler');
 
 var app = express();
 var port = process.env.PORT || 8080;
+
+mongoose.connect('mongobd://localhost/react-tweets');
+
+var twit = new twitter(config.twitter);
 
 var server = http.createServer(app).listen(port, function(){
     console.log('Express server listening on port' + port);
 });
 
+var io = require('socket.io').listen(server);
+
+twit.stream('statuses/filter'. {track: 'javascript, #react'}, function(steam){
+    streamHandler(stream, io);
+});
 
