075cf859ae0fccd0f650f20f2712439f93b1c18f
Dustin <dustinsinkey1@gmail.com>
2016-02-06 17:01:24 -0800

    Changed name of TweetApp and added routes.js file

---
+++ routes.js
@@ -0,0 +1,9 @@
+var JSX = require('node-jsx').install();
+var React = require('react');
+var TweetsApp = require('./components/TweetsApp.react.js');
+var Tweet = require('./models/Tweet.js');
+
+module.exports = {
+  index: function(req, res){
+  }
+}
