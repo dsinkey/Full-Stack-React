fa5c97e03877c5241df2963055197f03e8645bf9
Dustin <dustinsinkey1@gmail.com>
2016-02-08 17:53:42 -0800

    Corrected errors in streamHandler

--- utils/StreamHandler.js
+++ utils/StreamHandler.js
@@ -2,16 +2,17 @@ var Tweet = require('../models/Tweet');
 
 module.exports = function(stream, io){
   stream.on('data', function(data){
-
-    var tweet = {
-      twid: data['id'],
-      active: false,
-      author: data['user']['name'],
-      avatar: data['user']['profile_image_url'],
-      body: data['text'],
-      date: data['created_at'],
-      screenname: data['user']['screenname']
-    };
+    if (data['user' !== undefined]){
+      var tweet = {
+        twid: data['id_str'],
+        active: false,
+        author: data['user']['name'],
+        avatar: data['user']['profile_image_url'],
+        body: data['text'],
+        date: data['created_at'],
+        screenname: data['user']['screen_name']
+      };
+    }
 
     var tweetEntry = new Tweet(tweet);
 
@@ -22,5 +23,5 @@ module.exports = function(stream, io){
     });
 
   });
-  
+
 };
